4a9c391685cbeb941910165e517511db20d19b26
Anbin <z_anbin@163.com>
2026-06-18 19:17:53 +0800

    Expand page network request contracts

--- tests/e2e/page/page-network-request.contract.test.ts
+++ tests/e2e/page/page-network-request.contract.test.ts
@@ -118,6 +118,51 @@ describe("page network request contract e2e", () => {
     });
   });
 
+  it("overrides post data content type", async () => {
+    await withPage(async (page) => {
+      await page.goto(fixture.server.EMPTY_PAGE);
+      let serverRequestHeaders: Record<string, string | string[] | undefined> | null = null;
+      fixture.server.setRoute("/post", (request, response) => {
+        serverRequestHeaders = request.headers;
+        response.end();
+      });
+      await page.route("**/post", (route, request) => {
+        const headers = request.headers();
+        headers["content-type"] = "application/x-www-form-urlencoded; charset=UTF-8";
+        void route.continue({
+          headers,
+          postData: request.postData()
+        });
+      });
+      await page.evaluate(async () => {
+        await fetch("./post", { method: "POST", body: "foo=bar" });
+      });
+      expect(serverRequestHeaders).toBeTruthy();
+      expect(serverRequestHeaders!["content-type"]).toBe("application/x-www-form-urlencoded; charset=UTF-8");
+    });
+  });
+
+  it("gets null with postData() and postDataJSON() when there is no post data", async () => {
+    await withPage(async (page) => {
+      const response = await page.goto(fixture.server.EMPTY_PAGE);
+      expect(response!.request().postData()).toBe(null);
+      expect(response!.request().postDataJSON()).toBe(null);
+    });
+  });
+
+  it("parses form urlencoded post data", async () => {
+    await withPage(async (page) => {
+      await page.goto(fixture.server.EMPTY_PAGE);
+      fixture.server.setRoute("/post", (_request, response) => response.end());
+      const requestPromise = page.waitForRequest("**/post");
+      await page.setContent(
+        "<form method='POST' action='/post'><input type='text' name='foo' value='bar'><input type='number' name='baz' value='123'><input type='submit'></form>"
+      );
+      await page.click("input[type=submit]");
+      expect((await requestPromise).postDataJSON()).toEqual({ foo: "bar", baz: "123" });
+    });
+  });
+
   it("parses urlencoded post data", async () => {
     await withPage(async (page) => {
       await page.goto(fixture.server.EMPTY_PAGE);
@@ -145,6 +190,40 @@ describe("page network request contract e2e", () => {
       expect(requests.get("frame.html")!.isNavigationRequest()).toBe(true);
       expect(requests.get("script.js")!.isNavigationRequest()).toBe(false);
       expect(requests.get("style.css")!.isNavigationRequest()).toBe(false);
+    });
+  });
+
+  it("returns navigation bit when navigating to image", async () => {
+    await withPage(async (page) => {
+      const requests = [];
+      page.on("request", (request) => requests.push(request));
+      await page.goto(fixture.server.PREFIX + "/pptr.png").catch(() => null);
+      expect(requests[0]!.isNavigationRequest()).toBe(true);
+    });
+  });
+
+  it("returns eventsource resource type", async () => {
+    await withPage(async (page) => {
+      const sseMessage = { foo: "bar" };
+      fixture.server.setRoute("/sse", (_request, response) => {
+        response.writeHead(200, {
+          "Content-Type": "text/event-stream",
+          Connection: "keep-alive",
+          "Cache-Control": "no-cache"
+        });
+        response.write(`data: ${JSON.stringify(sseMessage)}\n\n`);
+      });
+
+      await page.goto(fixture.server.EMPTY_PAGE);
+      const requests = [];
+      page.on("request", (request) => requests.push(request));
+      expect(await page.evaluate(() => {
+        const eventSource = new EventSource("/sse");
+        return new Promise((resolve) => {
+          eventSource.onmessage = (event) => resolve(JSON.parse(event.data));
+        });
+      })).toEqual(sseMessage);
+      expect(requests[0]!.resourceType()).toBe("eventsource");
     });
   });
 });
